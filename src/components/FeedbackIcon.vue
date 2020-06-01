ac19c0534fbf10faea04f053855f6c02cfc12116
Ngoc Son Nguyen <nguyenngocson4391@gmail.com>
2020-06-01 22:36:21 +0700

    icon

---
+++ src/components/FeedbackIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <icon :type="type" v-bind:class="{selected: selected}" />
+</template>
+
+<script>
+import { Icon } from "ant-design-vue";
+export default {
+  name: "FeecbackIcon",
+  components: {
+    Icon
+  },
+  props: {
+    type: String,
+    selected: Boolean
+  }
+};
+</script>
+
+<style scoped>
+.selected {
+  background-color: red;
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     123456
+    <FeedbackIcon type="home" selected />
   </div>
 </template>
 
 <script>
+import FeedbackIcon from "./FeedbackIcon.vue"
+
 export default {
   name: 'Home',
+  components: {
+    FeedbackIcon
+  }
 }
 </script>
 
