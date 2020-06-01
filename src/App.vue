af9fcc9145d36ef71e4abd6d9194871e3271ef1d
Ngoc Son Nguyen <nguyenngocson4391@gmail.com>
2020-06-01 21:41:36 +0700

    antd

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home />
+    <DatePicker />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Home from "./components/Home.vue";
+import { DatePicker } from "ant-design-vue";
+import "ant-design-vue/dist/antd.css"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home,
+    DatePicker
   }
 }
 </script>
@@ -23,6 +27,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    123456
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+
+<style scoped>
+</style>
