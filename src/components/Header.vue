bf96462841aeff4d512d4923fa707d360f34f818
Son <nnson@IfiSolution.local>
2020-06-11 14:18:05 +0700

    menu mobile

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,55 +1,152 @@
 <template>
-<div>
-  <div class="header">
-    <img alt="logo" src="../assets/logo-big.png" class="header-logo" />
-    <div class="header-banner-text">{{$t('header.banner.text')}}</div>
-    <div class="header-nav">
-      <router-link to="/">Feedback</router-link>
-      <router-link to="/dashboard">Dashboard</router-link>
+  <div class="header-wrapper">
+    <div class="header">
+      <img alt="logo" src="../assets/logo-big.png" class="header-logo" />
+      <div class="header-banner-text">{{$t('header.banner.text')}}</div>
+      <div class="header-nav">
+        <router-link to="/">{{$t('header.nav.feedback')}}</router-link>
+        <router-link to="/dashboard">{{$t('header.nav.dashboard')}}</router-link>
+      </div>
+      <MobileMenu
+        class="header-nav-mobile"
+        :isClicked="isMobileMenuClicked"
+        @click.native="onMobileMenuClick"
+      />
+    </div>
+    <div class="header-banner-mobile-text">{{$t('header.banner.text')}}</div>
+    <div class="menu-mobile-wrapper" v-if="isMobileMenuClicked">
+      <div class="menu-mobile">
+        <router-link to="/" @click.native="onMobileMenuClick">{{$t('header.nav.feedback')}}</router-link>
+        <router-link to="/dashboard" @click.native="onMobileMenuClick">{{$t('header.nav.dashboard')}}</router-link>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
+import MobileMenu from "./MobileMenu";
+
 export default {
   name: "Header",
-  components: {}
+  components: {
+    MobileMenu
+  },
+  data: () => {
+    return {
+      isMobileMenuClicked: false
+    };
+  },
+  methods: {
+    onMobileMenuClick() {
+      this.isMobileMenuClicked = !this.isMobileMenuClicked;
+    }
+  }
 };
 </script>
 
 <style scoped lang="scss">
-.header {
-  height: $header-height;
-  position: fixed;
-  width: 100%;
-  background-color: aliceblue;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding-left: 20px;
-  font-size: 24px;
-  z-index: 999;
+.header-wrapper {
+  min-width: $min-width;
 
-  .header-logo {
-    height: 100%;
-  }
+  .header {
+    height: $header-height;
+    position: fixed;
+    width: 100%;
+    background-color: aliceblue;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-left: 20px;
+    font-size: 24px;
+    z-index: 999;
 
-  .header-banner-text {
-    text-transform: uppercase;
-  }
-
-  .header-nav {
-    padding: 30px;
-
-     a {
-      font-weight: bold;
-      color: #2c3e50;
-      margin-left: 20px;
+    .header-logo {
+      height: 100%;
     }
 
-    a.router-link-exact-active {
-      color: #42b983;
+    .header-banner-text {
+      text-transform: uppercase;
+      font-size: 22px;
+      font-weight: bold;
+    }
+
+    .header-nav {
+      padding-right: 30px;
+
+      a {
+        font-weight: bold;
+        color: #2c3e50;
+        margin-left: 20px;
+      }
+
+      a.router-link-exact-active {
+        color: #42b983;
+      }
+    }
+
+    .header-nav-mobile {
+      padding-right: 30px;
+    }
+  }
+
+  .header-banner-mobile-text {
+    padding-top: $header-height;
+    font-size: 15px;
+    font-weight: bold;
+    text-transform: uppercase;
+    text-align: center;
+    width: 100%;
+  }
+
+  .menu-mobile-wrapper {
+    font-size: 24px;
+    position: fixed;
+    top: $header-height;
+    width: 100%;
+    height: 90px;
+
+    .menu-mobile {
+      display: flex;
+      flex-direction: column;
+      width: 90%;
+      margin: auto;
+      background-color: aliceblue;
+      margin-top: 2px;
+      height: 100%;
+      justify-content: center;
+      border-radius: 10px;
+      box-shadow: 0 10px 15px 0 rgba(134, 134, 134, .15);
+
+      a {
+        font-weight: bold;
+        color: #2c3e50;
+        margin-left: 20px;
+      }
+
+      a.router-link-exact-active {
+        color: #42b983;
+      }
+    }
+
+  }
+
+  @media screen and (max-width: $pivot-width) {
+    .header-banner-text,
+    .header-nav {
+      display: none;
+    }
+
+    .header-banner-mobile-text,
+    .header-nav-mobile {
+      display: inline-block;
+    }
+  }
+
+  @media screen and (min-width: $pivot-width) {
+    .header-banner-mobile-text,
+    .header-nav-mobile,
+    .menu-mobile-wrapper {
+      display: none;
     }
   }
 }
---
+++ src/components/MobileMenu.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="mobile-menu-wrapper">
+    <div class="first-bar" v-bind:class="{clicked: isClicked}"></div>
+    <div class="second-bar" v-bind:class="{clicked: isClicked}"></div>
+    <div class="third-bar" v-bind:class="{clicked: isClicked}"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MobileMenu",
+  props: {
+    isClicked: Boolean
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.mobile-menu-wrapper {
+  display: inline-block;
+  cursor: pointer;
+
+  .first-bar,
+  .second-bar,
+  .third-bar {
+    width: 35px;
+    height: 5px;
+    background-color: #2c3e50;
+    margin: 6px 0;
+    transition: 0.4s;
+    position: relative;
+  }
+
+  .clicked.first-bar {
+    -webkit-transform: rotate(-45deg);
+    transform: rotate(-45deg);
+    top: 10px;
+  }
+
+  .clicked.second-bar {
+    opacity: 0;
+  }
+
+  .clicked.third-bar {
+    -webkit-transform: rotate(45deg);
+    transform: rotate(45deg);
+    top: -11px;
+  }
+}
+</style>
+
