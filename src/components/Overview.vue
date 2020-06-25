f4b85a2018bce2754544fa3b9a96a7e143104b7d
Ngoc Son Nguyen <nguyenngocson4391@gmail.com>
2020-06-25 09:49:42 +0700

    overview table

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -1,51 +1,57 @@
 <template>
-  <div class="over-view-all-prjs">
-    <div class="over-view-all-prjs-headers">
-      <div class="over-view-all-prjs-section"></div>
-      <div class="over-view-all-prjs-header"><img src="../assets/bronze.png" /></div>
-      <div class="over-view-all-prjs-header"><img src="../assets/silver.png" /></div>
-      <div class="over-view-all-prjs-header"><img src="../assets/gold.png" /></div>
-      <div class="over-view-all-prjs-header"><img src="../assets/platinum.png" /></div>
-    </div>
-    <div v-for="section in sections" :key="section.id" class="over-view-all-prjs-row">
-      <div>{{section.title}}</div>
-      <div>{{section.bronze || 0}}</div>
-      <div>{{section.silver || 0}}</div>
-      <div>{{section.gold || 0}}</div>
-      <div>{{section.platin || 0}}</div>
-    </div>
+  <div class="over-view-wrapper">
+    <Row>
+      <Col span="8"></Col>
+      <Col span="4" class="over-view-header"><img src="../assets/bronze.png" /></Col>
+      <Col span="4" class="over-view-header"><img src="../assets/silver.png" /></Col>
+      <Col span="4" class="over-view-header"><img src="../assets/gold.png" /></Col>
+      <Col span="4" class="over-view-header"><img src="../assets/platinum.png" /></Col>
+    </Row>
+    <Row v-for="section in sections" :key="section.id" class="over-view-row">
+      <Col class="over-view-section" span="8">{{section.title}}</Col>
+      <Col class="over-view-value" span="4">{{section.bronze || 0}}</Col>
+      <Col class="over-view-value" span="4">{{section.silver || 0}}</Col>
+      <Col class="over-view-value" span="4">{{section.gold || 0}}</Col>
+      <Col class="over-view-value" span="4">{{section.platin || 0}}</Col>
+    </Row>
   </div>
 </template>
 <script>
+import { Row, Col } from "ant-design-vue";
+
 export default {
   name: "Overview",
   props: {
     sections: Array
+  },
+  components: {
+    Row,
+    Col
   }
 };
 </script>
 <style scoped lang="scss">
-.over-view-all-prjs {
-  color: #000;
+.over-view-wrapper {
+  color: #999999;
+  font-size: 15px;
 
-  .over-view-all-prjs-section {
-    width: 25%;
+  .over-view-row {
+    height:30px;
+    margin-top: 10px;
+    border-bottom: 1px solid #f2f2f2;
   }
 
-  .over-view-all-prjs-headers {
-    display: flex;
-    justify-content: space-between;
+  .over-view-value,
+  .over-view-header {
+    text-align: center;
 
-    .over-view-all-prjs-header {
-      img {
-        width: 60px;
-      }
+    img {
+      width: 60px;
     }
   }
 
-  .over-view-all-prjs-row {
-    height: 30px;
-    display: flex;
+  .over-view-section {
+    color: #666666;
   }
 }
 </style>
